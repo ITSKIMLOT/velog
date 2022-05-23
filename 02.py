ae2f2432c0a5c38dc287a091b2e3897ad9afc34e
김럿 <imkimlot@kmonline.xyz>
2022-05-23 20:44:33 +0900

    봇의 기본 구조 만들기 예제파일
    봇의 기본 구조 만들기 예제파일입니다.

--- 02.py
+++ 02.py
@@ -4,10 +4,10 @@ from discord.ext import commands
 INTENTS = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=INTENTS)
 bot.remove_command(help)
-token = "OTc3NTkwMzE3MjE5OTc5MzI1.GSvOAT.Hm8Wz9uXP0MwHjtBgP_PUbtkvo5xSMz6z6cPMU"
+token = "토큰을 붙혀넣으세요!" # 메시지를 지우고, 본인의 토큰을 넣어주세요.
 
 @bot.event
-async def on_ready():
+async def on_ready(): # 봇의 준비를 감지합니다.
 	print("봇이 준비되었습니다!")
 
-bot.run(token)
+bot.run(token) # 봇을 실행시킵니다.
